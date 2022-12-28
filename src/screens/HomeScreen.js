be4a4bd5f58b11913b0fbecf18fa8ffb7ef039c8
Ioana Lazea <lazeaioanabianca@gmail.com>
2022-12-28 02:32:48 +0200

    Get user's daily goals.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, SafeAreaView, TouchableOpacity, View } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { colors } from '../utils/colors';
+import { Ionicons } from '@expo/vector-icons';
+import Footer from '../components/Footer';
 import { firebase } from '../firebase/config'
-
+import {
+  ref,
+  onValue
+} from 'firebase/database';
 const HomeScreen = ({navigation}) => {
     const handleSignOut = () => {
       firebase
@@ -16,7 +21,21 @@ const HomeScreen = ({navigation}) => {
         })
         .catch(error => alert(error.message))
     }
+
+
+    const [dailyGoals, setDailyGoals] = useState({});
   
+    //get all daily goals from database
+    useEffect(() => {
+      var goalsRef = firebase.database().ref('users/' + firebase.auth().currentUser.uid + "/goals")
+        goalsRef.orderByChild("type").equalTo("Daily").on("child_added", function(snapshot) {
+        const data = snapshot.val();
+        let myDailyGoals = {...data};
+        setDailyGoals(myDailyGoals);
+     });
+    }, []);
+  
+    console.log(dailyGoals);
     return (
       <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
             <KeyboardAwareScrollView style={styles.containerScreen}>
@@ -25,10 +44,11 @@ const HomeScreen = ({navigation}) => {
                     <View>
                         <Text style={styles.textDaily}>Daily</Text>
                         <Text style={styles.textGoals}>Goals</Text>
-                        <Text style={styles.userGreeting}>Hello, {firebase.auth().currentUser?.displayName}!</Text>
+                        <Text style={styles.userGreeting}>Hello, {firebase.auth().currentUser?.displayName}! <Ionicons name="sunny" size={24} color="black" /></Text>
+                       
                     </View>
                 </View>
-        <Text>Email: {firebase.auth().currentUser?.email}</Text>
+       
         <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('AddGoal')}>
           <Text style={styles.buttonText}>New goal</Text>
         </TouchableOpacity>
@@ -39,6 +59,7 @@ const HomeScreen = ({navigation}) => {
           <Text style={styles.buttonText}>Sign out</Text>
         </TouchableOpacity>
         </KeyboardAwareScrollView>
+        <Footer />
       </SafeAreaView>
     )
   }
